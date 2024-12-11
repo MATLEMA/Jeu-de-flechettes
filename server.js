587ea580e161b8daeb6d6fc8219c9ddb7c2e0a17
Mathéo <matheo.lemarie49@gmail.com>
2024-12-11 19:18:22 +0100

    Login et username handling
    . Le login est maintenant fonctionnel
    . Fabrication d'uuid afin de reconnaitre les utilisateurs.
    . Username dans le chat
    . NPM AUDIT FIX
    . Ajout Cookie-parser
    . Suppression express-session

--- server.js
+++ server.js
@@ -1,87 +1,108 @@
-
-var express = require('express');
+/************************ IMPORT *************************/
+var express = require("express");
 var { createServer } = require("node:http");
-const path = require('path');
+const path = require("path");
+const cookieParser = require("cookie-parser");
 var app = express();
 var server = createServer(app);
+var crypto = require("crypto")
 
+/************************ SETUP EXPRESS *************************/
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-const io = require('socket.io')(server, {
+/************************ SETUP SOCKET.IO *************************/
+const io = require("socket.io")(server, {
     cors: {
         origin: "http://localhost:8080",
         methods: ["GET", "POST"],
-        transports: ['websocket', 'polling'],
+        transports: ["websocket", "polling"],
         credentials: true
     },
     allowEIO3: true
 });
 
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.get('/', function (req, res) {
-    console.log("/\n" + JSON.stringify(req.body, null, 2));
-    res.render("pages/login", {});
+/************************ PAGES HANDLING *************************/
+app.get("/", (req, res) => {
+    if (req.cookies.uuid){
+        res.clearCookie("uuid")
+    }
+    return res.render("pages/login", {});
 });
 
-app.get('/jeu', function (req, res) {
-    console.log("/jeu\n" + JSON.stringify(req.body, null, 2));
-    res.render("pages/jeu", {});
-});
-
-// Envoi d'une page 404 si la page n'est pas trouvée
-// app.all('*', (req, res) => { 
-//     console.log("/404\n" + JSON.stringify(req.headers, null, 2));
-//     console.log("/404\n" + JSON.stringify(req.body, null, 2));
-//     res.status(404).render("pages/404", {});
-// })
-
-app.post("/login", function (req, res) {
-    console.log("/login\n" + JSON.stringify(req.body, null, 2));
-    let username = req.body.username;
-
-    if (username) {
-        res.redirect("/jeu");
-        res.end();
+app.get("/jeu", (req, res) => {
+    if (req.cookies.uuid && Object.keys(utilisateurs).includes(req.cookies.uuid)) {
+        return res.render("pages/jeu", {});
     } else {
-        res.send("Veuillez entrer un nom correct!");
-		res.end();
+        res.redirect("/")
+    }
+});
+
+/* Dictionnaire des utilisateurs */
+var utilisateurs = {}
+
+/* Gère les noms d'utilisateurs */
+app.post("/login", (req, res) => {
+    isValid = (username) => {
+        if (username.length <= 30) {
+            return true
+        } else {
+            return false
+        }
+    } 
+    var username = req.body.username;
+
+    if (username && !Object.values(utilisateurs).includes(username) && isValid(username)) {
+        const uuid = crypto.randomUUID()
+        utilisateurs[uuid] = {"username": username}
+        res.cookie("uuid", uuid)
+        res.redirect("/jeu");
+    } else {
+        res.status(400).send("Veuillez saisir un nom d'utilisateur correct!");
     }
 })
 
-// SOCKET CONNEXION
-var utilisateurs = {}
+
+/* PAGE 404 */
+app.all('*', (req, res) => { 
+    console.log("/404\n" + JSON.stringify(req.headers, null, 2));
+    console.log("/404\n" + JSON.stringify(req.body, null, 2));
+    res.status(404).render("pages/404", {});
+})
+
+/************************ SOCKET.IO LOGIQUE *************************/
 
 io.on("connection", (utilisateur) => {
 
-    utilisateur.on("join", function(name){
-        var usernameExiste = Object.values(utilisateurs).includes(name);
-        if (usernameExiste) {
-            if (usernameExiste) {
-                utilisateur.emit("reponse", "AlreadyExist");
-            } else {
-                utilisateur.emit("reponse", "NonCorrect");
-            }
-        } else {
-            utilisateurs[utilisateur.id] = name;
-            console.log(`${name} s'est connecté`);
-            console.log(`${utilisateur.id} s'est connecté`);
-            utilisateur.emit("redirect", "/jeu");
-        }
+    utilisateur.on("join", (uuid) => {
+        var username = utilisateurs[uuid]["username"]
+        utilisateurs[uuid]["socket.id"] = utilisateur.id
+        console.log(`${username} s'est connecté`);
+        utilisateur.emit("redirect", "/jeu");
     });
 
-    utilisateur.on("chat message", (msg) => {
-        console.log(`message: ${utilisateurs[utilisateur.id]}> ${msg}`);
-        io.emit("chat message", `${utilisateurs[utilisateur.id]}> ${msg}`);
+    utilisateur.on("chat message", (object) => {
+        var uuid = object["sender"]
+        var msg = object["msg"]
+        var username = utilisateurs[uuid]["username"]
+        console.log(`message: ${username}> ${msg}`);
+        io.emit("chat message", `${username}> ${msg}`);
     });
 
     utilisateur.on('disconnect', () => {
+        var username = null
+        for (const key in utilisateurs) {
+            if (utilisateurs[key]["socket.id"] == utilisateur.id) {
+                var username = utilisateurs[key]["username"]
+            }
+        }
+        console.log(`${username} s'est deconnecté`);
         delete utilisateurs[utilisateur.id]
-        console.log(`${utilisateur.id} s'est deconnecté`);
     })
 });
 
+/************************ LISTENING *************************/
 server.listen(8080);
 console.log('8080 is the magic port');
